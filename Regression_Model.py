6e2155438382cb0deec003df560c354d4179548a
Ming Gao <minggao92@gmail.com>
2017-10-26 23:12:30 -0400

    made orig_state a categorical and automated regression coeff extraction

--- Regression_Model.py
+++ Regression_Model.py
@@ -4,27 +4,27 @@
 
 from statsmodels.formula.api import ols
 import pandas as pd
-import numpy as np
 
 #Extract Data from Excel
 ltl_price = pd.read_excel('C:\HomeDepot_Excel_Files\ltl_price.xlsx')
 ltl_price.head()
-ltl_price = ltl_price[(ltl_price["tot_shp_wt"] >= 200) & (ltl_price["tot_shp_wt"] <= 4999) & (ltl_price["aprv_amt"] <=1000)]
+ltl_price = ltl_price[(ltl_price['tot_shp_wt'] >= 200) & (ltl_price['tot_shp_wt'] <= 4999) & (ltl_price['aprv_amt'] <=1000)]
 #ltl_price["tot_mile_cnt1"] = ltl_price["tot_mile_cnt"] - np.mean(ltl_price["tot_mile_cnt"])
 #ltl_price["tot_shp_wt1"] = ltl_price["tot_shp_wt"] - np.mean(ltl_price["tot_shp_wt"])
-ltl_price['tot_mile_wt'] = ltl_price["tot_mile_cnt"] * ltl_price["tot_shp_wt"]
+ltl_price['tot_mile_wt'] = ltl_price['tot_mile_cnt'] * ltl_price['tot_shp_wt']
+ltl_price['orig_state'] = ltl_price["orig_state"].astype('category')
+
 
 
 # fit our model with .fit() and show results
-linehaul_model = ols("aprv_amt ~ tot_mile_cnt + tot_shp_wt + orig_state + tot_mile_wt", data=ltl_price).fit()
+linehaul_model = ols('aprv_amt ~ tot_mile_cnt + tot_shp_wt + orig_state + tot_mile_wt', data=ltl_price).fit()
 # summarize our model
 linehaul_model_summary = linehaul_model.summary()
 print(linehaul_model_summary)
 
 #Terms & Coeff
-variables = ['intercept','GA','MD','OH','tot_shp_wt','tot_mile_cnt','tot_mile_wt']
-coeff = [linehaul_model.params.Intercept,linehaul_model.params["orig_state[T.GA]"],linehaul_model.params["orig_state[T.MD]"],linehaul_model.params["orig_state[T.OH]"],linehaul_model.params["tot_shp_wt"],linehaul_model.params["tot_mile_cnt"],linehaul_model.params["tot_mile_wt"]]
-
+variables = [linehaul_model.params.index.tolist()][0]
+coeff = [linehaul_model.params.tolist()][0]
 # Convert two lists to a dictionary
 dictionary = dict(zip(variables,coeff))
 
